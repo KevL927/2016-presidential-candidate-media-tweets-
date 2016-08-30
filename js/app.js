070256f82c4cf684cbe45e107fd301ed376158f4
KevL927 <leekevin@gmail.com>
2016-08-30 12:45:56 -0400

    layout of the project

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  function getRequest() {
+
+      var params = {
+
+      }
+
+  $.getJSON('https://api.twitter.com/1.1/search/tweets.json',
+    var params = {
+      q: ,
+      key: , 
+    };
+    )
+
+  }
+
+});
